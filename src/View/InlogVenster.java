2a6203f6dc90f3e931b5a54b3e4b56a94cd50ad0
Robin van den Udenhout <rlmudenh@avans.nl>
2018-04-18 10:59:23 +0200

    button

--- src/View/InlogVenster.java
+++ src/View/InlogVenster.java
@@ -2,14 +2,20 @@ package View;
 
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class InlogVenster extends JFrame {
+	
+	private JButton loginbutton;
 	 
 	public InlogVenster(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setPreferredSize(new Dimension(300,300));
+		loginbutton = new JButton("Inloggen");
+		add(loginbutton);
+		setResizable(false);
 		pack();
 		setVisible(true);
 	}
