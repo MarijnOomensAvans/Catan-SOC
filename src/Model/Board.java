d722a824d3f6c709d8e2f97a962e7de2b43b12bd
MarijnOomensAvans <mdoomens@avans.nl>
2018-04-25 10:03:46 +0200

    Begin loop

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -4,5 +4,13 @@ import java.util.HashMap;
 
 public class Board {
 	private HashMap<Integer,Collection> axisgrid = new HashMap<Integer,Collection>();
+	
+	public void generateBoard() {
+		for(int x = 1;x<12;x++) {
+			for(int y = 1; y<12;y++) {
+				
+			}
+		}
+	}
 
 }
