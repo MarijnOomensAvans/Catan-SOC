f2d37fab0c698175a788f84d8c49bd2163317886
Robin van den Udenhout <rlmudenh@avans.nl>
2018-04-25 10:16:37 +0200

    extra

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 
 public class Player {
 	
+	private int player_id;
+	private int game_id;
 	private String username;
+	private String password;
 	private Color color;
+	private int order_number;
 
 }
