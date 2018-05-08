1aa468e3918c24d1e1306d341ed75ddd241d1497
Jeroen <jaahendr@avans.nl>
2018-05-08 13:00:08 +0100

    Dice class DAL prep
    Small adjustments in preparation of the DAL compatibility with the Dice class.

--- src/Model/Dice.java
+++ src/Model/Dice.java
@@ -1,10 +1,26 @@
 package Model;
-
 import java.util.Random;
+import DAL.MainDAL;
 
 public class Dice {
 	private int dice = 1;
 	private Random rand = new Random();
+	private MainDAL dbConn = new MainDAL();
+	
+	public Dice() {
+		try {
+			//driver name to be added, currently unknown
+			//dbConn.loadDataBaseDriver(driverName);
+			
+			//No connection without driver, to be added
+			//dbConn.makeConnection();
+			
+			System.out.println("Database driver connection skipped, name unknown.");
+			System.out.println("No database connection possible, no driver");
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 	
 	public int getDice() {
 		return dice;
