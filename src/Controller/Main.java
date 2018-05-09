e80bb721301b016938b3093502df123f4e7f651b
bryandejong <bdjong1@avans.nl>
2018-05-09 12:39:05 +0200

    LobbyDAL bijgewerkt

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -7,12 +7,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		new LobbyFrame();
-		
+
 		boolean connected = false;
 		MainDAL mainDAL = new MainDAL();
-		
-		while(connected == false) {
-			if((mainDAL.loadDataBaseDriver("com.mysql.cj.jdbc.Driver")) && (mainDAL.makeConnection())) {
+
+		while (connected == false) {
+			if ((mainDAL.loadDataBaseDriver("com.mysql.cj.jdbc.Driver")) && (mainDAL.makeConnection())) {
 				connected = true;
 				System.out.println("Connected");
 			} else {
--- src/DAL/LobbyDAL.java
+++ src/DAL/LobbyDAL.java
@@ -6,28 +6,51 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import Model.GameInfo;
+
 public class LobbyDAL {
 
 	public LobbyDAL() {
-		
+
 	}
-	
+
 	public ArrayList<String> getAllAccounts() {
-		
 		ArrayList<String> accounts = new ArrayList<String>();
-		
 		try {
 			Connection conn = MainDAL.getConnection();
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT username FROM account");
-			while(rs.next()) {
+			while (rs.next()) {
 				accounts.add(rs.getString(1));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 		return accounts;
 	}
 	
+	public ArrayList<GameInfo> getAllActiveGames(String username) {
+		ArrayList<GameInfo> games = new ArrayList<GameInfo>();
+		try {
+			Connection conn = MainDAL.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+						"SELECT sp.idspel, sb.username"
+						+ " FROM account AS a"
+						+ " JOIN speler AS s ON a.username = s.username"
+						+ " JOIN spel AS sp ON sp.idspel = s.idspel"
+						+ " JOIN speler AS sb ON sp.beurt_idspeler = sb.idspeler"
+						+ " WHERE s.username = '" + username + "'"
+					);
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String userTurn = rs.getString(2);
+				games.add(new GameInfo(id, userTurn));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return games;
+	}
+
 }
