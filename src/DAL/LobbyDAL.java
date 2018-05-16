c44b29c0d68bd0bf2504ff188324f40189654980
bryandejong <bdjong1@avans.nl>
2018-05-16 14:29:19 +0200

    LobbyDAL

--- src/DAL/LobbyDAL.java
+++ src/DAL/LobbyDAL.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import Controller.LoginController;
 import Model.LobbyGameInfo;
+import Model.LobbyInvite;
 
 public class LobbyDAL {
 
@@ -44,10 +45,8 @@ public class LobbyDAL {
 					+ "JOIN speler sp "
 					+ "ON s.idspel = sp.idspel "
 					+ "WHERE sp.username LIKE '" + LoginController.getUsername() + "' "
-					+ "AND sp.speelstatus NOT LIKE UITGE");
-			/*
-			 * Get all users in game
-			 */
+					+ "AND (sp.speelstatus LIKE 'uitdager' OR sp.speelstatus LIKE 'geaccepteerd')");
+			
 			while(rs.next()) {
 				int gameID = rs.getInt(1);
 				ArrayList<String> players = getUsersInGame(gameID);
@@ -58,6 +57,29 @@ public class LobbyDAL {
 			e.printStackTrace();
 		}
 		return games;
+	}
+	
+	public ArrayList<LobbyInvite> getAllInvites(){
+		ArrayList<LobbyInvite> invites = new ArrayList<LobbyInvite>();
+		try {
+			Connection conn = MainDAL.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+					"SELECT s.idspel FROM spel s JOIN speler sp ON s.idspel = sp.idspel " + 
+					"WHERE sp.username LIKE '" + LoginController.getUsername() + "' " + 
+					"AND sp.speelstatus LIKE 'uitgedaagde'"
+					);
+			while(rs.next()) {
+				int gameID = rs.getInt(1);
+				String host = getHost(gameID);
+				invites.add(new LobbyInvite(gameID, host));
+			}
+			stmt.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return invites;
 	}
 	
 	private ArrayList<String> getUsersInGame(int gameID){
@@ -82,6 +104,25 @@ public class LobbyDAL {
 		return players;
 	}
 	
-	
+	private String getHost(int gameID) {
+		try {
+			Connection conn = MainDAL.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+					"SELECT sp.username FROM spel s "
+					+ "JOIN speler sp ON s.idspel = sp.idspel "
+					+ "WHERE sp.speelstatus LIKE 'uitdager' "
+					+ "AND s.idspel = " + gameID
+					);
+			
+			while(rs.next()) {
+				return rs.getString(1);
+			}
+			stmt.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return "dbErr: host not found";
+	}
 
 }
