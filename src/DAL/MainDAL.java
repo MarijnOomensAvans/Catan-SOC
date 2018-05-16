b4b704dce778f7065885a18c3a7b5927e355e840
Robin van den Udenhout <rlmudenh@avans.nl>
2018-05-16 15:19:05 +0200

    DB shit

---
+++ src/DAL/MainDAL.java
@@ -0,0 +1,51 @@
+package DAL;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MainDAL {
+	private static Connection conn;
+
+	public MainDAL()
+	{
+		conn = null;
+	}
+
+	public boolean loadDataBaseDriver(String driverName)
+	{
+		try
+		{
+			// Load the JDBC driver
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e)
+		{
+			// Could not find the database driver
+			System.out.println("ClassNotFoundException : " + e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	public boolean makeConnection()
+	{
+		try
+		{
+			conn = DriverManager
+					.getConnection("jdbc:mysql://databases.aii.avans.nl/bdjong1_db2?user=bdjong1&password=Ab12345");
+		
+		} catch (SQLException ex)
+		{
+			// handle any errors
+			
+			return false;
+		}
+		return true;
+	}
+	
+	public static Connection getConnection() {
+		return conn;
+	}
+}
+	
