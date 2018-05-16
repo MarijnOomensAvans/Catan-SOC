88f449b051874c0dec402e55db065de8142fb248
Robin van den Udenhout <rlmudenh@avans.nl>
2018-05-16 15:26:10 +0200

    c

--- src/DAL/ChatDal.java
+++ src/DAL/ChatDal.java
@@ -1,5 +1,13 @@
 package DAL;
 
+import java.sql.Connection;
+
 public class ChatDal {
+	
+	Connection conn;
+	
+	public ChatDal() {
+		conn = MainDAL.getConnection();
+	}
 
 }
--- src/view/Chatinput.java
+++ src/view/Chatinput.java
@@ -1,9 +1,15 @@
 package view;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import DAL.ChatDal;
+import DAL.MainDAL;
 
 @SuppressWarnings("serial")
 public class Chatinput extends JPanel {
@@ -13,8 +19,31 @@ public class Chatinput extends JPanel {
 	
 	public Chatinput(){
 		chatinput.setColumns(30);
+		sendbutton.addActionListener(new ActionListener() {
+
+			
+			public void actionPerformed(ActionEvent e) {
+				SendMessage();
+				
+			}
+
+			
+		});
+		
+		
 		add(chatinput);
 		add(sendbutton);
 	}
 
+	private void SendMessage() {
+		MainDAL db = new MainDAL();
+		{ 
+		if ((db.loadDataBaseDriver("com.mysql.jdbc.Driver"))
+				&& (db.makeConnection()))
+		{
+			ChatDal cd = new ChatDal();
+		
+	}
+}
+	}
 }
