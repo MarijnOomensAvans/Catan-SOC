82aa0ac5f99aa672edda5d08f8754a0d1f85f146
juraj336 <jgrietbe@avans.nl>
2018-05-23 10:10:39 +0200

    StartFrame

--- src/View/PlayFrame.java
+++ src/View/PlayFrame.java
@@ -1,8 +1,18 @@
 package View;
 
-public class PlayFrame {
+import javax.swing.JFrame;
+
+public class PlayFrame extends JFrame{
 	
-	
+	public PlayFrame() {
+		setTitle("Lobby");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		//setContentPane(pane);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
 	
 	
 	
