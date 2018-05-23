de05466a16dd1e076e7c9dcf29ed47ca0d85f630
Glenn <38490259+GlennVanAlphen@users.noreply.github.com>
2018-05-23 11:16:35 +0200

    Can paint images

--- src/Model/MainClass.java
+++ src/Model/MainClass.java
@@ -2,15 +2,16 @@ package Model;
 
 import Controller.BankController;
 import Controller.PlayerController;
-
+import Controller.TradeController;
 import DAL.MainDAL;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		MainDAL md = new MainDAL();
-		BankController bc = new BankController(md);
-		new PlayerController(md,bc,1);
+//		BankController bc = new BankController(md);
+//		new PlayerController(md,bc,1);
+		new TradeController();
 
 	}
 }
--- src/View/TradeContentPane.java
+++ src/View/TradeContentPane.java
@@ -5,13 +5,21 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class TradeContentPane extends JPanel {
 	
-	private BufferedImage myBufferedImage = new BufferedImage(1,1,BufferedImage.TYPE_INT_RGB);
+	private BufferedImage myBufferedImage1;
+	private BufferedImage myBufferedImage2;
+	private BufferedImage myBufferedImage3;
+	private BufferedImage myBufferedImage4;
+	private BufferedImage myBufferedImage5;
+	
 	private File stone;
 	private File ore;
 	private File wood;
@@ -25,15 +33,29 @@ public class TradeContentPane extends JPanel {
 		wood = new File("./Images/hout.jpg");
 		wheat = new File("./Images/graan.jpg");
 		wool = new File("./Images/schaap.jpg");
-		setPreferredSize(new Dimension(500,500));
+			
+		try {
+				myBufferedImage1 = ImageIO.read(stone);
+				myBufferedImage2 = ImageIO.read(ore);
+				myBufferedImage3 = ImageIO.read(wood);
+				myBufferedImage4 = ImageIO.read(wheat);
+				myBufferedImage5 = ImageIO.read(wool);
+			}catch (IOException e) {
+			System.out.println(e.getMessage());
+			}	
+		this.repaint();
+		
+		setPreferredSize(new Dimension(600,600));
 		setVisible(true);
 	}
 	
-	
-	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-
+		g.drawImage(myBufferedImage1, 100, 200, 100, 100, null);
+		g.drawImage(myBufferedImage2, 200, 200, 100, 100, null);
+		g.drawImage(myBufferedImage3, 300, 200, 100, 100, null);
+		g.drawImage(myBufferedImage4, 400, 200, 100, 100, null);
+		g.drawImage(myBufferedImage5, 500, 200, 100, 100, null);
 	}
 
 }
