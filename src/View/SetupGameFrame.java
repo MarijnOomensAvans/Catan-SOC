698e3521ae84901f446e8a987ebe1b1205eb0c00
juraj336 <jgrietbe@avans.nl>
2018-05-28 14:11:26 +0200

    Removed Trash

--- src/View/SetupGameFrame.java
+++ src/View/SetupGameFrame.java
@@ -16,12 +16,7 @@ public class SetupGameFrame extends JFrame {
 
 		//Initialize the panels
 		setupPane = new SetupGamePane(lc, bc, lc.getGameID(), inviteButton);
-<<<<<<< HEAD
-		invitePane = new InvitePanel(lc);
-		
-=======
 		invitePane = new InvitePanel(lc, this);
->>>>>>> 5c78a1acf98b92c8c8be528c4df142608edfe462
 		this.setContentPane(setupPane);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
