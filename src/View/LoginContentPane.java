19363c072166813606d84d9a6fc6276b2846b8f2
bryandejong <bdjong1@avans.nl>
2018-05-28 14:22:17 +0200

    Comments

--- src/View/LoginContentPane.java
+++ src/View/LoginContentPane.java
@@ -30,7 +30,7 @@ public class LoginContentPane extends JPanel {
 			String password = loginPanel.getPassword();
 			controller.buttonPressedLogin(username, password);
 		});
-		
+
 		registerButton.addActionListener(e -> {
 			controller.buttonPressedRegister();
 			switchScreenRegister();
@@ -40,7 +40,7 @@ public class LoginContentPane extends JPanel {
 			controller.buttonPressedCancel();
 			switchScreenInlog();
 		});
-		
+
 		registerCompleetButton.addActionListener(e -> {
 			String username = registerPanel.getUsername();
 			String password = registerPanel.getPassword();
@@ -51,20 +51,24 @@ public class LoginContentPane extends JPanel {
 		add(loginPanel);
 	}
 
+	//Switch the visible panel to register
 	public void switchScreenRegister() {
 		removeAll();
 		repaint();
 		add(registerPanel);
 		GUI.pack();
 	}
-
+	
+	//Switch the visible panel to login
 	public void switchScreenInlog() {
 		removeAll();
 		repaint();
 		add(loginPanel);
 		GUI.pack();
 	}
+
 	
+	//Display the relevant warning for logging in / registering
 	public void setWarning(String warning) {
 		loginPanel.setWarning(warning);
 		registerPanel.setWarning(warning);
--- src/View/SetupGameFrame.java
+++ src/View/SetupGameFrame.java
@@ -7,21 +7,15 @@ import Controller.BoardController;
 import Controller.LobbyController;
 
 public class SetupGameFrame extends JFrame {
-	
+
 	private SetupGamePane setupPane;
 	private InvitePanel invitePane;
-	
-	
+
 	public SetupGameFrame(LobbyController lc, BoardController bc, JButton inviteButton) {
 
-		//Initialize the panels
+		// Initialize the panels
 		setupPane = new SetupGamePane(lc, bc, lc.getGameID(), inviteButton);
-<<<<<<< HEAD
-		invitePane = new InvitePanel(lc);
-		
-=======
 		invitePane = new InvitePanel(lc, this);
->>>>>>> 5c78a1acf98b92c8c8be528c4df142608edfe462
 		this.setContentPane(setupPane);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
@@ -30,13 +24,13 @@ public class SetupGameFrame extends JFrame {
 		this.setLocationRelativeTo(null);
 	}
 
-	//Set the content pane to show invite panel
+	// Set the content pane to show invite panel
 	public void openInvitePanel() {
 		this.setContentPane(invitePane);
 		updateFrame();
 	}
-	
-	//Repaint and validate the frame
+
+	// Repaint and validate the frame
 	public void updateFrame() {
 		this.repaint();
 		this.validate();
