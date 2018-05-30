b8eb57559ed7108d1163cd884619b25995bf0bf1
mussaf <38488831+mussaf@users.noreply.github.com>
2018-05-30 14:45:38 +0200

    invites

--- src/Model/lobby/LobbyGameState.java
+++ src/Model/lobby/LobbyGameState.java
@@ -4,14 +4,26 @@ import java.util.ArrayList;
 
 public class LobbyGameState {
 
-	int gameid;
-	ArrayList<String> players;
-	ArrayList<String> status;
+	private int gameid;
+	private ArrayList<String> players;
+	private ArrayList<String> status;
 	
 	public LobbyGameState(int gameid, ArrayList<String> players, ArrayList<String> status) {
 		this.gameid = gameid;
 		this.players = players;
 		this.status = status;
+	}
+
+	public int getGameid() {
+		return gameid;
+	}
+
+	public ArrayList<String> getPlayers() {
+		return players;
+	}
+
+	public ArrayList<String> getStatus() {
+		return status;
 	}
 	
 }
