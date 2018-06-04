ba4e6c3f731c18e2d2ad628e8533aa53898598fd
MarijnOomensAvans <mdoomens@avans.nl>
2018-06-04 13:45:39 +0200

    Error hotfix

--- src/View/build/BuildFrame.java
+++ src/View/build/BuildFrame.java
@@ -7,7 +7,7 @@ public class BuildFrame extends JFrame{
 	private BuildContentPane buildcontentpane;
 	
 	public BuildFrame() {
-		this.buildcontentpane = new BuildContentPane(this);
+		this.buildcontentpane = new BuildContentPane(this, null); //TODO geef player mee als er een beurtensysteem in zit
 		this.setTitle("Bouw");
 		setResizable(false);
 		this.setContentPane(buildcontentpane);
