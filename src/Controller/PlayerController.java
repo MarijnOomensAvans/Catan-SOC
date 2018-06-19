d270fd50bd8ac288489288c859023b646890b567
juraj336 <jgrietbe@avans.nl>
2018-06-19 14:32:38 +0200

    FixedShizzleMdizzle

--- src/Controller/PlayerController.java
+++ src/Controller/PlayerController.java
@@ -220,11 +220,13 @@ public class PlayerController {
 		String allBuildings = "";
 		if (buildingType.equals("Village")) {
 			pieceID = "d0";
-		    allBuildings = allBuildings + "," + db.getBuilding(playerID, pieceID);
+			if(allBuildings.equals("")) {
+				allBuildings = db.getBuilding(playerID, pieceID);
+			}else {
+				allBuildings = allBuildings + "," + db.getBuilding(playerID, pieceID);
+			}
 			String[] keys = allBuildings.split(",");
 			String[] coords = hlPoint.split(",");
-			System.out.println(keys[0]);
-			System.out.println(keys.length);
 			if (keys.length != 5) {
 				switch (keys.length) {
 				case 1:
