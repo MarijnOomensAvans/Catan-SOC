27a9ff1985ffbdfcea56df25ecfe13864c93c024
MarijnOomensAvans <mdoomens@avans.nl>
2018-06-21 12:25:37 +0200

    MainCLass veranderd

--- src/Controller/MainClass.java
+++ src/Controller/MainClass.java
@@ -1,14 +1,9 @@
 package Controller;
 
-import DAL.MainDAL;
-import DAL.RobberDAL;
-
 public class MainClass {
 
 	public static void main(String[] args) {
-		new MainDAL();
-		RobberDAL rb = new RobberDAL();
-		System.out.println(rb.robberHasPosition(7, 5, 11));
+		new LoginController();
 	}
 
 }
